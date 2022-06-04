f707cb8fb43497ad36331c2af2bc7e2c26c34948
Eva van Houten <user@server.fake>
2022-06-04 11:43:33 +0200

    Completed day 2 part 2 (for loop needed :()

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -21,9 +21,38 @@ fn main() {
     let total_depth = sum_amounts(down) - sum_amounts(up);
 
     println!(
-        "Submarine depth is {}, horizontal position is {}, multiplied: {}",
+        "Part 1: Submarine depth is {}, horizontal position is {}, multiplied: {}",
         total_depth,
         total_forward,
         total_depth * total_forward
-    )
+    );
+
+    let mut aim = 0;
+    let mut total_forward = 0;
+    let mut total_depth = 0;
+
+    for line in input.lines() {
+        let line: Vec<&str> = line.split(' ').collect();
+        let direction = line[0];
+        let amount = line[1]
+            .parse::<usize>()
+            .expect("Something went wrong parsing the input to a number");
+
+        match direction {
+            "up" => aim -= amount,
+            "down" => aim += amount,
+            "forward" => {
+                total_forward += amount;
+                total_depth += amount * aim;
+            }
+            _ => panic!("Invalid command in input"),
+        }
+    }
+
+    println!(
+        "Part 2: Submarine depth is {}, horizontal position is {}, multiplied: {}",
+        total_depth,
+        total_forward,
+        total_depth * total_forward
+    );
 }
